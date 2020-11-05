c9cb07d672832a3c6d23e89d942cef78be11f423
Conrad S <conrad.schloer@gmail.com>
2020-11-05 12:57:29 +0100

    Add security group to rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,12 +1,15 @@
 resource "aws_db_instance" "redmine" {
-  identifier           = "redmine"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "redmine"
-  username             = "admin"
-  password             = var.rds_password
-  parameter_group_name = "default.mysql5.7"
+  identifier                = "redmine"
+  allocated_storage         = 20
+  storage_type              = "gp2"
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  instance_class            = "db.t3.micro"
+  name                      = "redmine"
+  username                  = "admin"
+  password                  = var.rds_password
+  parameter_group_name      = "default.mysql5.7"
+    vpc_security_group_ids  = [aws_security_group.redmine.id]
+  skip_final_snapshot       = true
+
 }
