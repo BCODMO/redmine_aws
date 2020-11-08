11b142378a82235ca2ddbad3be0d1c4848ed623a
Conrad S <conrad.schloer@gmail.com>
2020-11-08 18:18:14 +0100

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -12,15 +12,31 @@ then
 fi
 
 
+USE_CACHE=false
+read -p "Use the cache for docker build? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    USE_CACHE=true
+fi
+
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 504672911985.dkr.ecr.us-east-1.amazonaws.com/redmine_$ENV
-echo "Pulling"
-docker pull redmine:3.4.6
+
+
+if [ "$USE_CACHE" = true ]
+then
+    echo "Building with cache"
+    docker-compose build redmine
+else
+    echo "Building without cache"
+    docker-compose build --no-cache redmine
+fi
 echo "Tagging"
-docker tag redmine:3.4.6 504672911985.dkr.ecr.us-east-1.amazonaws.com/redmine_$ENV:$VERSION
+docker tag redmine_aws_redmine:latest 504672911985.dkr.ecr.us-east-1.amazonaws.com/redmine_$ENV:$VERSION
 echo "Pushing"
 docker push 504672911985.dkr.ecr.us-east-1.amazonaws.com/redmine_$ENV:$VERSION
 
 if [ "$REDEPLOY"  = true ]
 then
-    export AWS_PAGER="" && aws ecs update-service --force-new-deployment --service redmine_$ENV --cluster submission-$ENV
+    export AWS_PAGER="" && aws ecs update-service --force-new-deployment --service redmine_$ENV --cluster redmine-$ENV
 fi
