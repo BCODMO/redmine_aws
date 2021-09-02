28c9bba2a725aac5a2c483cd2319d00db9b0f212
Conrad S <conrad.schloer@gmail.com>
2021-09-02 23:22:15 +0200

    Improve JS and add line to dockerfile to change delete message

--- Dockerfile
+++ Dockerfile
@@ -22,6 +22,14 @@ RUN bundle install --without development test
 
 # Add max height and scroll to DM Processing Notes custom field
 RUN echo "\n\n/*Add max height and scroll to DM Processing Notes custom field */\ndiv.cf_18.attribute div.value {\n  overflow-y: auto;\n  max-height: 300px;\n}" >> /usr/src/redmine/public/stylesheets/application.css
+
+# Add custom code to issues page
 COPY js/osprey.js /usr/src/osprey.js
 RUN cat /usr/src/osprey.js >> /usr/src/redmine/public/javascripts/application.js
-#RUN rm /usr/src/osprey.js
+RUN rm /usr/src/osprey.js
+
+# Change warning text of deleting an issue
+
+RUN sed -i 's/Are you sure you want to delete the selected issue(s)?/Are you sure you want to delete the entire issue(s)? Deleted issues cannot be restored./g' /usr/src/redmine/config/locales/sq.yml 
+RUN sed -i 's/Are you sure you want to delete the selected issue(s)?/Are you sure you want to delete the entire issue(s)? Deleted issues cannot be restored./g' /usr/src/redmine/config/locales/en-GB.yml 
+RUN sed -i 's/Are you sure you want to delete the selected issue(s)?/Are you sure you want to delete the entire issue(s)? Deleted issues cannot be restored./g' /usr/src/redmine/config/locales/en.yml 
--- js/osprey.js
+++ js/osprey.js
@@ -1,38 +1,55 @@
 // Custom injected JS to add osprey info
 function addOspreyInfo() {
-  // Check that we're in the right path
-  const match = window.location.pathname.match(/^\/issues\/([0-9]*)$/);
-  if (match.length === 2) {
-    const issueId = match[1];
-    $("div#content div.attributes")
-      .first()
-      .append('<div id="bcodmo-osprey-query"></div>');
-
-    fetch(
-      `https://private.bco-dmo.org/sparql?default-graph-uri=&query=SELECT+DISTINCT+%3Furi+IF%28STRLEN%28%3Ftitle%29+%3E+0%2C+%3Ftitle%2C+%3Fshort_title%29+as+%3Flabel%0D%0AWHERE+%7B%0D%0A++%0D%0A++GRAPH+%3Chttps%3A%2F%2Fd2rq.bco-dmo.org%2Fid%2Fredmine%2F%3E+%7B+%0D%0A++++%3Furi+odo%3AtrackingIssue+%3Fissue+.%0D%0A++++%3Fissue+odo%3AidentifierValue+%22${issueId}%22%5E%5Exsd%3Atoken+.%0D%0A++++%3Fissue+odo%3AidentifierScheme+odo%3AIdentifierScheme_RedmineIssueTracker+.%0D%0A++%7D%0D%0A++%7B%0D%0A++++SERVICE+%3Chttps%3A%2F%2Flod.bco-dmo.org%2Fsparql%3E+%7B%0D%0A++++++%7B%0D%0A++++++++%3Furi+a+odo%3ADataset+.%0D%0A++++++++OPTIONAL+%7B%3Furi+odo%3AdatasetTitle+%3Ftitle+.%7D%0D%0A++++++++%3Furi+dcterms%3Atitle+%3Fshort_title+.%0D%0A++++++%7D+%0D%0A++++++UNION+%0D%0A++++++%7B%0D%0A++++++++%3Furi+a+odo%3AProject+.%0D%0A++++++++%3Furi+dcterms%3Atitle+%3Ftitle+.%0D%0A++++++++OPTIONAL+%7B%3Furi+odo%3AhasAcronym+%3Fshort_title+.%7D%0D%0A++++++%7D%0D%0A++++%7D%0D%0A++%7D%0D%0A%7D&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on`
-    ).then(async (response) => {
-      const data = await response.json();
-      for (let key in data.results.bindings) {
-        // Make sure the results look like we expect
-        if (
-          "uri" in data.results.bindings[key] &&
-          "label" in data.results.bindings[key]
-        ) {
-          newEl = $(`
-            <div>
-              <hr/><strong>OSPREY:</strong> <a
-                href="${data.results.bindings[key].uri.value}"
-                target="_blank">
-                ${data.results.bindings[key].label.value}
-              </a>
-            </div>
-
-          `);
-          $("#bcodmo-osprey-query").append(newEl);
+  try {
+    // Check that we're in the right path
+    const match = window.location.pathname.match(/^\/issues\/([0-9]*)$/);
+    if (match && match.length === 2) {
+      const issueId = match[1];
+      // Make the request
+      fetch(
+        `https://private.bco-dmo.org/sparql?default-graph-uri=&query=SELECT+DISTINCT+%3Furi+STR%28%3Fshort_title%29+as+%3Fshort_title+IF%28STRLEN%28%3Ftitle%29+%3E+0%2C+STR%28%3Ftitle%29%2C+%22%22%29+as+%3Ftitle%0D%0AWHERE+%7B%0D%0A++%0D%0A++GRAPH+%3Chttps%3A%2F%2Fd2rq.bco-dmo.org%2Fid%2Fredmine%2F%3E+%7B+%0D%0A++++%3Furi+odo%3AtrackingIssue+%3Fissue+.%0D%0A++++%3Fissue+odo%3AidentifierValue+%22${issueId}%22%5E%5Exsd%3Atoken+.%0D%0A++++%3Fissue+odo%3AidentifierScheme+odo%3AIdentifierScheme_RedmineIssueTracker+.%0D%0A++%7D%0D%0A%7B%0D%0A++++SERVICE+%3Chttps%3A%2F%2Flod.bco-dmo.org%2Fsparql%3E+%7B%0D%0A++++++%7B%0D%0A++++++++%3Furi+a+odo%3ADataset+.%0D%0A++++++++OPTIONAL+%7B%3Furi+odo%3AdatasetTitle+%3Ftitle+.%7D%0D%0A++++++++%3Furi+dcterms%3Atitle+%3Fshort_title+.%0D%0A++++++%7D+%0D%0A++++++UNION+%0D%0A++++++%7B%0D%0A++++++++%3Furi+a+odo%3AProject+.%0D%0A++++++++%3Furi+dcterms%3Atitle+%3Ftitle+.%0D%0A++++++++OPTIONAL+%7B%3Furi+odo%3AhasAcronym+%3Fshort_title+.%7D%0D%0A++++++%7D%0D%0A++++%7D%0D%0A++%7D%0D%0A%7D&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on`
+      ).then(async (response) => {
+        const data = await response.json();
+        if (data.results.bindings.length > 0) {
+          // Only append the html when the document is ready
+          $(document).ready(() => {
+            try {
+              $("div#content div.attributes")
+                .first()
+                .append('<div id="bcodmo-osprey-query"></div>');
+              const newWrapper = $(
+                "<div><hr/><strong>OSPREY:</strong><br></div>"
+              );
+              data.results.bindings.forEach((r) => {
+                // Make sure the results look like we expect
+                if ("uri" in r && "short_title" in r) {
+                  const uri = r.uri.value;
+                  const short_title = r.short_title.value;
+                  newEl = $(`
+                  <div>
+                    ${short_title} <a
+                      href="${uri}"
+                      target="_blank">
+                      ${uri}
+                    </a>
+                  </div>
+                `);
+                  newWrapper.append(newEl);
+                }
+              });
+              $("#bcodmo-osprey-query").append(newWrapper);
+            } catch (e) {
+              console.warn("Failed in addOspreyInfo appending HTML");
+              console.warn(e);
+            }
+          });
         }
-      }
-    });
+      });
+    }
+  } catch (e) {
+    console.warn("Failed in addOspreyInfo");
+    console.warn(e);
   }
 }
 
-$(document).ready(addOspreyInfo);
+addOspreyInfo();
